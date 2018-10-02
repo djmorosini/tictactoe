79bdffce04f078204a249b4adbec15b0f79f9dfa
djmorosini <djmorosini@outlook.com>
2018-10-02 14:36:24 -0400

    updated logic in main.js

--- main.js
+++ main.js
@@ -127,12 +127,12 @@ function computerTurn() {
     handleClick(5)
   } else if (turn === 'o' && five === 'x' && one === '1') {
     handleClick(1)
-  } else if (turn === 'o' && five === 'x' && nine === 'x' && seven === '7') {
+  } else if (turn === 'o' && (one === 'x' && eight === 'x' || five === 'x' && nine === 'x' || four === 'x' && eight === 'x') && seven === '7') {
     handleClick(7)
-  } else if (turn === 'o' && (two ==='x' && four === 'x' || two ==='x' && six === 'x' || six ==='x' && eight === 'x') && three === '3') {
-    handleClick(3)
-  } else if (turn === 'o' && (four ==='x' && eight === 'x') && nine === '9') {
+  } else if (turn === 'o' && (six === 'x' && eight === 'x' || three === 'x' && eight === 'x') && nine === '9') {
     handleClick(9)
+  } else if (turn === 'o' && (two === 'x' && four === 'x' || two === 'x' && six === 'x' || six === 'x' && eight === 'x') && three === '3') {
+    handleClick(3)
   } else if (turn === 'o' && two === '2') {
     handleClick(2)
   } else if (turn === 'o' && four === '4') {
